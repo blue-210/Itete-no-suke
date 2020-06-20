0679e545458b15e39c9691fae41936bbb3eac8aa
青木隼人 <captain-blue@MacBook-Pro.local>
2020-06-21 00:31:58 +0900

    Remember-me機能を追加

--- src/main/java/app/itetenosuke/controller/user/LoginController.java
+++ src/main/java/app/itetenosuke/controller/user/LoginController.java
@@ -1,14 +1,22 @@
 package app.itetenosuke.controller.user;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import app.itetenosuke.domain.user.model.UserDetailsImpl;
 
 @Controller
 public class LoginController {
 
 	@GetMapping("/login")
-	public String getLogin(Model model) {
+	public String getLogin(Model model
+			, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+		if(userDetails != null) {
+			model.addAttribute("contents", "home/home :: home_contents");
+			return "home/homeLayout";
+		}
 		
 		return "login/login";
 	}
