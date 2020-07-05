2ce40e7a8b8801235fcb883148b9a111a3e22143
captain-blue210 <captain.blue210@gmail.com>
2020-07-05 19:58:44 +0900

    Postgresが起動するまで待つよう修正

---
+++ wait-for-postgres.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until psql -h "$host" -U "postgres" -c '\l'; do
+  echo >&2 "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+echo >&2 "Postgres is up - executing command"
+exec $cmd
