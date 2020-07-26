aed6ae3cc2bdcf1b1180702ae4fbf3beffdd98d6
captain-blue210 <captain.blue210@gmail.com>
2020-07-27 01:10:19 +0900

    fix:ci-cd.yml

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # outputディレクトリ内のファイルをrsync over SSHで転送
-rsync -acvz --delete ./ sukeroku@itetenosuke.com:itetenosuke/
+rsync -acvz --delete ./src sukeroku@itetenosuke.com:itetenosuke/
 # サービスを再起動
 ssh sukeroku@itetenosuke.com "cd itetenosuke/; docker-compose build; docker-compose down; docker-compose -f docker-compose.yml -f docker-compose-prod.yml up -d"
