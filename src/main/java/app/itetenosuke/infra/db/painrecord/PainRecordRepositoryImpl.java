6f493f5ab7f995ca2aff944a2993968a3f43200c
captain-blue210 <captain.blue210@gmail.com>
2021-04-22 23:30:06 +0900

    feat : Split the jooq project

---
+++ src/main/java/app/itetenosuke/infra/db/painrecord/PainRecordRepositoryImpl.java
@@ -0,0 +1,18 @@
+package app.itetenosuke.infra.db.painrecord;
+
+import java.util.Optional;
+
+import app.itetenosuke.domain.painrecord.PainRecord;
+import app.itetenosuke.domain.painrecord.PainRecordRepository;
+
+public class PainRecordRepositoryImpl implements PainRecordRepository {
+	
+	private static final PAIN_RECORDS_TABLE P = PAIN_R
+
+	@Override
+	public Optional<PainRecord> findById(Long painRecordId) {
+		
+		return null;
+	}
+
+}
