c3fd81330cfd7e35fa03669ff04be9af80508323
captain-blue210 <captain.blue210@gmail.com>
2021-04-24 08:07:59 +0900

    test : add update a record test

--- src/test/java/app/itetenosuke/api/application/painrecord/PainRecordUseCaseTest.java
+++ src/test/java/app/itetenosuke/api/application/painrecord/PainRecordUseCaseTest.java
@@ -1,82 +1,82 @@
 package app.itetenosuke.api.application.painrecord;
 
-import java.time.LocalDateTime;
-import javax.persistence.EntityManager;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import app.itetenosuke.application.painrecord.PainRecordUseCase;
-import app.itetenosuke.domain.painrecord.PainRecordRepository;
-import app.itetenosuke.infrastructure.db.painrecord.PainRecordDataModel;
-import app.itetenosuke.presentation.model.PainRecordRequest;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 
+import app.itetenosuke.application.painrecord.PainRecordUseCase;
+import app.itetenosuke.domain.painrecord.PainLevel;
+import app.itetenosuke.domain.painrecord.PainRecord;
+import app.itetenosuke.presentation.model.PainRecordRequest;
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
 @Transactional
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-    DirtiesContextTestExecutionListener.class, TransactionDbUnitTestExecutionListener.class})
-@ExtendWith(SpringExtension.class)
-// @DataJpaTest
+@TestExecutionListeners({
+  DependencyInjectionTestExecutionListener.class,
+  DirtiesContextTestExecutionListener.class,
+  TransactionDbUnitTestExecutionListener.class
+})
+@Slf4j
 class PainRecordUseCaseTest {
-  @Autowired
-  private PainRecordUseCase painRecordUseCase;
-
-  @Autowired
-  private EntityManager entityManager;
-
-  @Autowired
-  private PainRecordRepository painRecordRepository;
+  @Autowired private PainRecordUseCase painRecordUseCase;
 
   @Test
   @DisplayName("痛み記録を1件取得できる")
   @DatabaseSetup("/painrecord/setup_get_a_record.xml")
-  @ExpectedDatabase(value = "/painrecord/expected_get_a_record.xml", table = "pain_records")
   public void testGetPainRecord() {
-    PainRecordDataModel result = painRecordUseCase.getPainRecord(1L);
-  }
-
-  @Test
-  @DisplayName("痛み記録を1件登録できる")
-  @DatabaseSetup(type = DatabaseOperation.DELETE_ALL,
-      value = "/painrecord/setup_create_a_record.xml")
-  @ExpectedDatabase(value = "/painrecord/expected_create_a_record.xml", table = "pain_records",
-      assertionMode = DatabaseAssertionMode.NON_STRICT)
-  public void testCreatePainRecord() {
-    PainRecordRequest req = new PainRecordRequest();
-    req.setPainLevel(2);
-    req.setMemo("create test");
-    req.setCreatedAt(LocalDateTime.now());
-    req.setUpdatedAt(LocalDateTime.now());
-    PainRecordDataModel result = painRecordUseCase.createPainRecord(req);
-    // JPAやHibernateを使った更新系のテストの場合、永続コンテキストを明示的にフラッシュする必要がある
-    entityManager.flush();
+    PainRecord result = painRecordUseCase.getPainRecord(1L);
+    assertAll(
+        "result",
+        () -> assertThat(result.getPainRecordID(), is(1L)),
+        () -> assertThat(result.getMemo(), is("test")));
   }
 
   @Test
   @DisplayName("痛み記録を1件更新できる")
-  @DatabaseSetup("/painrecord/setup_update_a_record.xml")
-  @ExpectedDatabase(value = "/painrecord/expected_update_a_record.xml", table = "pain_records",
+  @DatabaseSetup(value = "/painrecord/setup_update_a_record.xml")
+  @ExpectedDatabase(
+      value = "/painrecord/expected_update_a_record.xml",
+      table = "pain_records",
       assertionMode = DatabaseAssertionMode.NON_STRICT)
-  public void testUpdatePainRecord() {
+  public void testCreatePainRecord() {
     PainRecordRequest req = new PainRecordRequest();
     req.setPainRecordID(1L);
-    req.setPainLevel(2);
+    req.setPainLevel(PainLevel.VERY_SEVERE_PAIN.getCode());
     req.setMemo("update test");
+    req.setCreatedAt(LocalDateTime.now());
     req.setUpdatedAt(LocalDateTime.now());
-    PainRecordDataModel result = painRecordUseCase.updatePainRecord(req);
-    entityManager.flush();
+    painRecordUseCase.updatePainRecord(req);
   }
+  //
+  //  @Test
+  //  @DisplayName("痛み記録を1件更新できる")
+  //  @DatabaseSetup("/painrecord/setup_update_a_record.xml")f
+  //  @ExpectedDatabase(value = "/painrecord/expected_update_a_record.xml", table = "pain_records",
+  //      assertionMode = DatabaseAssertionMode.NON_STRICT)
+  //  public void testUpdatePainRecord() {
+  //    PainRecordRequest req = new PainRecordRequest();
+  //    req.setPainRecordID(1L);
+  //    req.setPainLevel(2);
+  //    req.setMemo("update test");
+  //    req.setUpdatedAt(LocalDateTime.now());
+  //    PainRecordDataModel result = painRecordUseCase.updatePainRecord(req);
+  //    entityManager.flush();
+  //  }
 }
