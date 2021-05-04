4d145dd665323558e57c9a5f8eaa4db5dcb13b5b
Aoki Hayato <67002842+captain-blue210@users.noreply.github.com>
2021-05-04 22:19:36 +0900

    feat : implements get bodypart list function (#84)

---
+++ infra/src/main/resources/db/migration/V1_0_9__add_user_id_bodyparts.sql
@@ -0,0 +1,4 @@
+ALTER TABLE
+   sukeroku.body_parts
+ADD
+   COLUMN user_id char(36);
---
+++ src/main/java/app/itetenosuke/api/presentation/controller/bodypart/BodyPartController.java
@@ -0,0 +1,31 @@
+package app.itetenosuke.api.presentation.controller.bodypart;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.itetenosuke.api.application.bodypart.BodyPartUseCase;
+import app.itetenosuke.api.presentation.controller.shared.BodyPartResBody;
+import app.itetenosuke.domain.user.model.UserDetailsImpl;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class BodyPartController {
+  private final BodyPartUseCase bodyPartUseCase;
+
+  @GetMapping(path = "/v1/bodyparts", produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<BodyPartResBody> getBodyPartList(
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+    return bodyPartUseCase
+        .getBodyPartList(userDetails.getUserId())
+        .stream()
+        .map(v -> BodyPartResBody.of(v))
+        .collect(Collectors.toList());
+  }
+}
