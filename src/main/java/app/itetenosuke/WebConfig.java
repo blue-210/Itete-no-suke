20ff20f98f28f3691bdd7b2fbd9cbc2ea0040a92
captain-blue210 <captain.blue210@gmail.com>
2021-05-10 21:44:58 +0900

    fix : Always return the response as json

--- src/main/java/app/itetenosuke/WebConfig.java
+++ src/main/java/app/itetenosuke/WebConfig.java
@@ -1,10 +1,15 @@
 package app.itetenosuke;
 
+import java.util.List;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.MediaType;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.accept.FixedContentNegotiationStrategy;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -26,4 +31,8 @@ public class WebConfig implements WebMvcConfigurer {
     return localValidatorFactoryBean;
   }
 
+  @Override
+  public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+    configurer.strategies(List.of(new FixedContentNegotiationStrategy(MediaType.APPLICATION_JSON)));
+  }
 }
